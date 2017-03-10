7ee8226be0166605531a8f7fb5cf68363c9a1511
penaandrew12 <penaandrew12@yahoo.com>
2017-03-09 19:41:30 -0800

    Signed-off-by: penaandrew12 <penaandrew12@yahoo.com>

--- Homework/HW Assignment #2/HW_Assignment_#2/main.cpp
+++ Homework/HW Assignment #2/HW_Assignment_#2/main.cpp
@@ -2,7 +2,7 @@
  * File:   main.cpp
  * Author: Andrew Pena
  * Created on March 9, 2017, 4:00 PM
- * Purpose:
+ * Purpose: Degree Calculations
  */
 
 //System Libraries
@@ -18,17 +18,20 @@ using namespace std; //Name-space under which system libraries exist
 //Execution begins here
 int main(int argc, char** argv) {
     //Declare variables
-    float FVLoan=1000.00f,  //Face Value in Dollars
-            IntrstRte=.15,  //Interest Rate from percent
-            Mnths=18;       //Duration Kept in Months
+    float F,        //Fahrenheit
+          C;      //Celsius
     
     //Initialize variables
-    
+    cout<<"Celsius = ";
+    cin>>C;     
+            
     //Input data
+    F=32+1.8*C;
     
     //Map inputs to outputs or process the data
     
     //Output the transformed data
+    cout<<"Fahrenheit = "<<F<<" degrees"<<endl;
     
     //Exit stage!
     return 0;
