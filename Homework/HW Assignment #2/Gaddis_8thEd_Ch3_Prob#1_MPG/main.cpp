36604161b71ad8d194f8df9c86fef874dde2bf03
penaandrew12 <penaandrew12@yahoo.com>
2017-03-09 18:30:26 -0800

    Signed-off-by: penaandrew12 <penaandrew12@yahoo.com>

---
+++ Homework/HW Assignment #2/Gaddis_8thEd_Ch3_Prob#1_MPG/main.cpp
@@ -0,0 +1,42 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Pena
+ * Created on March 9, 2017, 3:51 PM
+ * Purpose:
+ */
+
+//System Libraries
+#include <iostream>  //Input - Output Library
+using namespace std; //Name-space under which system libraries exist
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare variables
+    float GalHld,
+            MlsDrvn,
+            MPG;
+    
+    //Initialize variables
+    cout<<"Full Tank of Gas in Gallons = ";
+    cin>>GalHld;
+    cout<<"Miles Driven on Full Tank = ";
+    cin>>MlsDrvn;
+    
+    //Input data
+    MPG=MlsDrvn/GalHld;
+    
+    //Map inputs to outputs or process the data
+    
+    //Output the transformed data
+    cout<<"Miles Driven Per Gallon = "<<MPG<<endl;
+    
+    //Exit stage!
+    return 0;
+}
+
