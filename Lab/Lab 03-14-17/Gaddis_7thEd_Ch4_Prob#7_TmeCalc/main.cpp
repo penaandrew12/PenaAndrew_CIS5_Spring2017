1c678cdeacff75ba9929dc42858ee09ac908de4e
penaandrew12 <penaandrew12@yahoo.com>
2017-03-14 11:37:22 -0700

    Signed-off-by: penaandrew12 <penaandrew12@yahoo.com>

--- Lab/Lab 03-14-17/Gaddis_7thEd_Ch4_Prob#7_TmeCalc/main.cpp
+++ Lab/Lab 03-14-17/Gaddis_7thEd_Ch4_Prob#7_TmeCalc/main.cpp
@@ -1,8 +1,8 @@
 /* 
  * File:   main.cpp
  * Author: Andrew Pena
- * Created on February 28, 2016, 11:10 AM
- * Purpose:
+ * Created on March 14, 2017, 11:30 AM
+ * Purpose: Time Calculation
  */
 
 //System Libraries 
@@ -12,6 +12,12 @@ using namespace std;
 //User Libraries 
 
 //Global Constants Only
+const int MINUTE=60;       //How many second in a minute
+const int HOUR=60*MINUTE;  //How many seconds in a hour
+const int DAY=24*HOUR;     //How many seconds in a day
+const int WEEK=7*DAY;      //How many seconds in a week
+const int YEAR=52*WEEK;    //How many seconds in a year
+const int MONTH=YEAR/12;   //How many seconds in an average month
 
 //Like PI, e, Gravity, or conversions
 
