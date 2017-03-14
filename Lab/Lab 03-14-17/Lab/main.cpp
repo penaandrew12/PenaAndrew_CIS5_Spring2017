5d31502dc453634c38702daa30b29c7a285bcc1c
penaandrew12 <penaandrew12@yahoo.com>
2017-03-14 11:18:27 -0700

    Signed-off-by: penaandrew12 <penaandrew12@yahoo.com>

---
+++ Lab/Lab 03-14-17/Lab/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Pena
+ * Created on February 28, 2016, 11:10 AM
+ * Purpose:
+ */
+
+//System Libraries 
+#include <iostream>
+using namespace std;
+
+//User Libraries 
+
+//Global Constants Only
+
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes
+
+//Program Execution Begins
+int main(int argc, char** argv) {
+    //Declare all Variables
+    
+    //Input or initialize values
+    
+    //Process/Calculations
+    
+    //Output Located
+
+    //Exit Stage
+    return 0;
+}
+
