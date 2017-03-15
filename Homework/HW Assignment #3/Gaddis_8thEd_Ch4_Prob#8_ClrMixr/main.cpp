b5b9d96185a196313e279af8978048826dafa990
Andrew Pena <Andrew Pena>
2017-03-14 23:21:43 -0700

    Signed-off-by: Andrew Pena <Andrew Pena>

---
+++ Homework/HW Assignment #3/Gaddis_8thEd_Ch4_Prob#8_ClrMixr/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Pena
+ * Created on March 14, 2017, 7:51 PM
+ * Purpose: Color Mixer
+ */
+
+//System Libraries
+#include <iostream>  //Input - Output Library
+using namespace std; //Name-space under which system libraries exist
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare variables
+    
+    //Initialize variables
+    
+    //Input data
+    
+    //Map inputs to outputs or process the data
+    
+    //Output the transformed data
+    
+    //Exit stage!
+    return 0;
+}
+
--- Homework/HW Assignment #3/Gaddis_8thEd_Ch4_Prob#8_ClrMixr/nbproject/Makefile-Release.mk
+++ Homework/HW Assignment #3/Gaddis_8thEd_Ch4_Prob#8_ClrMixr/nbproject/Makefile-Release.mk
@@ -23,7 +23,7 @@ AS=as
 # Macros
 CND_PLATFORM=Cygwin-Windows
 CND_DLIB_EXT=dll
-CND_CONF=Debug
+CND_CONF=Release
 CND_DISTDIR=dist
 CND_BUILDDIR=build
 
@@ -62,10 +62,10 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cpp_template.exe: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cpp_template ${OBJECTFILES} ${LDLIBSOPTIONS}
 
-${OBJECTDIR}/main.o: main.cpp
+${OBJECTDIR}/main.o: main.cpp 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
-	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
 
 # Subprojects
 .build-subprojects:
@@ -73,6 +73,7 @@ ${OBJECTDIR}/main.o: main.cpp
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cpp_template.exe
 
 # Subprojects
 .clean-subprojects:
