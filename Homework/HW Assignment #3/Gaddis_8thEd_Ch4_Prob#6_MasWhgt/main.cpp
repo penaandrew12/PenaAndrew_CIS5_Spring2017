23411987744970d9ace4e93106b20d4d01003e19
Andrew Pena <Andrew Pena>
2017-03-14 23:18:45 -0700

    Signed-off-by: Andrew Pena <Andrew Pena>

---
+++ Homework/HW Assignment #3/Gaddis_8thEd_Ch4_Prob#6_MasWhgt/main.cpp
@@ -0,0 +1,45 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Pena
+ * Created on March 13, 2017, 6:31 PM
+ * Purpose: Mass and Weight Calculation
+ */
+
+//System Libraries
+#include <iostream>  //Input - Output Library
+using namespace std; //Name-space under which system libraries exist
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare variables
+    int Wght,Mas;   //Weight of Object, Mass of Object
+    
+    //Output the transformed data
+    cout<<"Mass of Object = ";
+    cin>>Mas;
+    
+    Wght=Mas*9.8f;
+    
+    if (10>Wght){
+        cout<<"If object is less then 10 newtons ";
+        cout<<"it is too light to weigh\n";
+    }
+    else   
+        if (1000<Wght){
+            cout<<"If object is greater than 1000 newtons ";
+            cout<<"it is too heavy to weigh\n";
+        } 
+        else
+            
+    cout<<"Weight of Object is = "<<Wght<<" newtons"<<endl;
+    
+    //Exit stage!
+    return 0;
+}
+
