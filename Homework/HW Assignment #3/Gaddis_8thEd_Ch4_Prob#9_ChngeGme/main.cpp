8f938ba83fbc7f8cadc44ad3120fbb73cdad2cef
Andrew Pena <Andrew Pena>
2017-03-16 17:21:47 -0700

    Signed-off-by: Andrew Pena <Andrew Pena>

---
+++ Homework/HW Assignment #3/Gaddis_8thEd_Ch4_Prob#9_ChngeGme/main.cpp
@@ -0,0 +1,72 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Pena
+ * Created on March 9, 2017, 3:51 PM
+ * Purpose: Change for Dollar Game
+ */
+
+//System Libraries
+#include <iostream>  //Input - Output Library
+using namespace std; //Name-space under which system libraries exist
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare variables
+    float Penny,
+            Dime,
+            Nikl,
+            Qrtr,
+            VPenny,
+            VDime,
+            VNikl,
+            VQrtr;
+    float Dllr;
+    
+    cout<<"Let's Play A Game! Enter a certain amount"
+            " of coins to reach exactly 1 dollar.\n";
+    cout<<"Pennies = ";
+    cin>>Penny;
+    cout<<"Dime = ";
+    cin>>Dime;
+    cout<<"Nickels = ";
+    cin>>Nikl;
+    cout<<"Quarter = ";
+    cin>>Qrtr;
+    
+    VPenny=Penny*.01f;
+    VDime=Dime*.10f;
+    VNikl=Nikl*.05f;
+    VQrtr=Qrtr*.25f;
+    
+    //Map inputs to outputs or process the data
+    Dllr=VPenny+VDime+VNikl+VQrtr;
+    
+
+    //Output the transformed data
+    if(Dllr='1'){
+    cout<<"Congratulations! You Win! You Got Exactly One Dollar.\n";
+    cout<<"$"<<Dllr<<endl;}
+    else
+        if(Dllr>'1'){
+        cout<<"Sorry to Inform you, but you are not a winner.\n"
+                "You Went Over a dollar.\n"
+                "Try Again.\n";
+        cout<<"$"<<Dllr<<endl;}
+        else
+            if(Dllr<'1'){
+            cout<<"Sorry to Inform you, but you are not a winner.\n"
+                "You were less than a dollar.\n"
+                "Try Again.\n";
+            cout<<"$"<<Dllr<<endl;}
+            else
+                
+    //Exit stage!
+    return 0;
+}
+
---
+++ Homework/HW Assignment #3/Truth/main.cpp
@@ -0,0 +1,96 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Pena
+ * Created on March 9, 2017, 3:51 PM
+ * Purpose: Truth Table
+ */
+
+//System Libraries
+#include <iostream>  //Input - Output Library
+using namespace std; //Name-space under which system libraries exist
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare variables
+    bool x,y;
+    
+    //Output the table header
+    cout<<"X Y !X !Y X||Y X&&Y X^Y X^Y^Y X^Y^X !(X||Y) !X&&!Y !(X&&Y) !X||!Y"<<endl; 
+    
+    //Map inputs to outputs or process the data
+    x=true;y=true;
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<"  ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"   ";
+    cout<<(x||y?'T':'F')<<"    ";
+    cout<<(x&&y?'T':'F')<<"   ";
+    cout<<(x^y?'T':'F')<<"    ";
+    cout<<(x^y^y?'T':'F')<<"     ";
+    cout<<(x^y^x?'T':'F')<<"      ";
+    cout<<(!(x||y)?'T':'F')<<"       ";
+    cout<<(!x&&!y?'T':'F')<<"      ";
+    cout<<(!(x&&y)?'T':'F')<<"       ";
+    cout<<(!x||!y?'T':'F')<<" ";
+    cout<<endl;
+    
+    x=true;y=false;
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<"  ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"   ";
+    cout<<(x||y?'T':'F')<<"    ";
+    cout<<(x&&y?'T':'F')<<"   ";
+    cout<<(x^y?'T':'F')<<"    ";
+    cout<<(x^y^y?'T':'F')<<"     ";
+    cout<<(x^y^x?'T':'F')<<"      ";
+    cout<<(!(x||y)?'T':'F')<<"       ";
+    cout<<(!x&&!y?'T':'F')<<"      ";
+    cout<<(!(x&&y)?'T':'F')<<"       ";
+    cout<<(!x||!y?'T':'F')<<" ";
+    cout<<endl;
+    
+    x=false;y=true;
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<"  ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"   ";
+    cout<<(x||y?'T':'F')<<"    ";
+    cout<<(x&&y?'T':'F')<<"   ";
+    cout<<(x^y?'T':'F')<<"    ";
+    cout<<(x^y^y?'T':'F')<<"     ";
+    cout<<(x^y^x?'T':'F')<<"      ";
+    cout<<(!(x||y)?'T':'F')<<"       ";
+    cout<<(!x&&!y?'T':'F')<<"      ";
+    cout<<(!(x&&y)?'T':'F')<<"       ";
+    cout<<(!x||!y?'T':'F')<<" ";
+    cout<<endl;
+    
+    x=false;y=false;
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<"  ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"   ";
+    cout<<(x||y?'T':'F')<<"    ";
+    cout<<(x&&y?'T':'F')<<"   ";
+    cout<<(x^y?'T':'F')<<"    ";
+    cout<<(x^y^y?'T':'F')<<"     ";
+    cout<<(x^y^x?'T':'F')<<"      ";
+    cout<<(!(x||y)?'T':'F')<<"       ";
+    cout<<(!x&&!y?'T':'F')<<"      ";
+    cout<<(!(x&&y)?'T':'F')<<"       ";
+    cout<<(!x||!y?'T':'F')<<" ";
+    cout<<endl;
+    
+    //Output the transformed data
+    
+    //Exit stage!
+    return 0;
+}
+
