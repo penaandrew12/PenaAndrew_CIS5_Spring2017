d3149d0491a23db80ad5ddc0e00b0d01bf6bf5e5
penaandrew12 <penaandrew12@yahoo.com>
2017-03-20 14:01:45 -0700

    Signed-off-by: penaandrew12 <penaandrew12@yahoo.com>

--- Homework/HW Assignment #3/Gaddis_8thEd_Ch4_Prob#8_ClrMixr/main.cpp
+++ Homework/HW Assignment #3/Gaddis_8thEd_Ch4_Prob#8_ClrMixr/main.cpp
@@ -20,27 +20,35 @@ int main(int argc, char** argv) {
     //Declare variables
     char Y, B, R;            //Yellow, Blue, Red
     char P, O, G,            //Purple, Orange, Green
+         MxdClr,             //Mixed Color
          FrstClr, ScndClr;   //First Color, Second Color
   
     
     //Map inputs to outputs or process the data
-    cout<<"Of the Three Primary Colors [Y]ellow, [R]ed, [B]lue)";
-    cout<<" find out what colors form from being mixed of two\n";
+    cout<<"Of the Three Primary Colors [Y]ellow, [R]ed, [B]lue";
+    cout<<" find out what colors form from being mixed of two.\n";
+    cout<<"If you do not input Blue as B, Red as R, or Yellow as Y, ";
+    cout<<"An error message will appear."<<endl;
     cout<<"First Color = ";
     cin>>FrstClr;
     cout<<"Second Color = ";
     cin>>ScndClr;
 
-    if (FrstClr==R&&ScndClr==B){
+    MxdClr=FrstClr*ScndClr;
+    P=R*B;
+    O=R*Y;
+    G=B*Y;
+            
+    if (P){
     cout<<"If Red and Blue mix it makes Purple";}
             
-        else if(FrstClr==R&&ScndClr==Y){
+        else if(O){
         cout<<"If Blue and Yellow mix it makes Orange";}   
                
-            else if(FrstClr==B&&ScndClr==Y){
+            else if(G){
             cout<<"If Blue and Yellow mix it makes Green";}
     
-                    else if (!(R&&Y&&B)){
+                    else{
                     cout<<"Can not be determined with given colors";}
     
     //Exit stage!
