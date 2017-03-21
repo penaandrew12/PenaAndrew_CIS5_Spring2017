cfa55ad840235afa232f21250448331efd7e1530
penaandrew12 <penaandrew12@yahoo.com>
2017-03-20 21:03:47 -0700

    Signed-off-by: penaandrew12 <penaandrew12@yahoo.com>

--- Homework/HW Assignment #3/Gaddis_8thEd_Ch4_Prob#8_ClrMixr/main.cpp
+++ Homework/HW Assignment #3/Gaddis_8thEd_Ch4_Prob#8_ClrMixr/main.cpp
@@ -20,12 +20,11 @@ int main(int argc, char** argv) {
     //Declare variables
     char Y, B, R;            //Yellow, Blue, Red
     char P, O, G,            //Purple, Orange, Green
-         MxdClr,             //Mixed Color
          FrstClr, ScndClr;   //First Color, Second Color
   
     
     //Map inputs to outputs or process the data
-    cout<<"Of the Three Primary Colors [Y]ellow, [R]ed, [B]lue";
+    cout<<"Of the Three Primary Colors [Y]ellow, [R]ed, [B]lue,";
     cout<<" find out what colors form from being mixed of two.\n";
     cout<<"If you do not input Blue as B, Red as R, or Yellow as Y, ";
     cout<<"An error message will appear."<<endl;
@@ -34,22 +33,25 @@ int main(int argc, char** argv) {
     cout<<"Second Color = ";
     cin>>ScndClr;
 
-    MxdClr=FrstClr*ScndClr;
-    P=R*B;
-    O=R*Y;
-    G=B*Y;
-            
-    if (P){
-    cout<<"If Red and Blue mix it makes Purple";}
-            
-        else if(O){
-        cout<<"If Blue and Yellow mix it makes Orange";}   
-               
-            else if(G){
-            cout<<"If Blue and Yellow mix it makes Green";}
-    
-                    else{
-                    cout<<"Can not be determined with given colors";}
+        if (FrstClr=='Y'){
+        if (ScndClr=='B'){
+        cout<<"If you mix Yellow and Blue it will make Green"<<endl;}
+        if (ScndClr=='R'){  
+        cout<<"If you mix Yellow and Red it will make Orange"<<endl;}
+        }
+            else if (FrstClr=='B'){
+                if (ScndClr=='R'){
+                cout<<"If you mix Blue and Red it will make Purple."<<endl;}
+                if (ScndClr=='Y'){
+                cout<<"If you mix Blue and Yellow it will make Green"<<endl;}
+            }
+                    else if (FrstClr=='R'){
+                        if (ScndClr=='Y'){
+                        cout<<"If you mix Red and Yellow it will make Orange"<<endl;}
+                        if (ScndClr=='B'){
+                        cout<<"If you mix Red and Blue it will make Purple."<<endl;}
+                    }
+                        else cout<<"Invalid Color(s)."<<endl;
     
     //Exit stage!
     return 0;
