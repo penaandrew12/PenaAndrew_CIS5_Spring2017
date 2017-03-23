cb1ac7c9bec3d80c49492df0edca3d43193b4e20
penaandrew12 <penaandrew12@yahoo.com>
2017-03-23 11:38:25 -0700

    Signed-off-by: penaandrew12 <penaandrew12@yahoo.com>

--- Lab/Lab 03-23-17/CPPTemplate/main.cpp
+++ Lab/Lab 03-23-17/CPPTemplate/main.cpp
@@ -2,7 +2,7 @@
  * File:   main.cpp
  * Author: Andrew Pena
  * Created on March 23, 2017, 11:30 AM
- * Purpose: 
+ * Purpose: Sum 1 to N
  */
 
 //System Libraries 
@@ -19,14 +19,18 @@ using namespace std;
 
 //Program Execution Begins
 int main(int argc, char** argv) {
-    //Declare all Variables
+    //Declare all Initialize Variables
+    int n=10000, sum=0;
     
-    //Input or initialize values
-    
-    //Process/Calculations
+    //Input values
+    for(int i=1;1<=n;i++){
+        sum+=i;
+    }
     
     //Output Located
-
+    cout<<"the computed sum of 1 to "<<n<<" = "<<sum<<endl;
+    cout<<"and "<<n<<"*("<<n<<"+1/2="<<n*(n+1)/2<<endl;
+    
     //Exit Stage
     return 0;
 }
