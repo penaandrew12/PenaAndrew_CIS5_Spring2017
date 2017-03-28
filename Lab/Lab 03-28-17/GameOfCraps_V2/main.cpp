bec7437c96056e9c73c40eb3f07df1a27cc9fe12
penaandrew12 <penaandrew12@yahoo.com>
2017-03-28 12:21:50 -0700

    Signed-off-by: penaandrew12 <penaandrew12@yahoo.com>

---
+++ Lab/Lab 03-28-17/GameOfCraps_V2/main.cpp
@@ -0,0 +1,83 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Pena
+ * Created on March 28, 2017, 11:41 AM
+ * Purpose: Simulate a Craps Game
+ */
+
+//System Libraries Here
+#include <iostream>     //Library
+#include <ctime>        //Time to set the seed
+#include <cstdlib>      //Srand and rand function
+#include <fstream>      //File IO
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variable
+//Like PI, e, Gravity, or conversions
+const float PERCENT=100.f;  //Conversion to Percent
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed
+    srand(static_cast<unsigned int>(time(0)));
+    
+    //Declare all Variables Here
+    ifstream in;
+    int nGames=0,Wins=0,Losses=0;
+    
+    //Initialize
+    in.open("GameInfo.dat");
+    while(in>>nGames); //Last Value becomes number of games
+    
+    //Play Game
+    for(int game=1;game<=nGames;game++){
+        //Throw dice and sum
+        char die1=rand()%6+1;// [1,6]
+        char die2=rand()%6+1;// [1,6]
+        char sum1=die1+die2;
+        //Determine win
+        switch(sum1){
+            case 7:
+            case 11:Wins++;break;
+            case 2:
+            case 3:
+            case 12:Losses++;break;
+            default:{
+                //Loop until a 7 or previous sum is Given
+                bool thrwAgn=true;
+                do{
+                    //Throw the Dice Again
+                    char die1=rand()%6+1;// [1,6]
+                    char die2=rand()%6+1;// [1,6]
+                    char sum2=die1+die2;
+                    if(sum2==7){
+                            Losses++;
+                            thrwAgn=false;
+                    }else if (sum1==sum2){
+                            Wins++;
+                            thrwAgn=false;
+                    }
+                }while(thrwAgn);
+            }  
+        }
+    }
+    
+    //Output Located Here
+    cout<<"Total Number of Games         = "<<nGames<<endl;
+    cout<<"Number of Games Won           = "<<Wins<<endl;
+    cout<<"Number of Games Loss          = "<<Losses<<endl;
+    cout<<"Total Games of Wins and Loses = "<<Wins+Losses<<endl;
+    cout<<"Percentage Wins               = "
+            <<static_cast<float>(Wins)/nGames*PERCENT<<"%"<<endl;
+    cout<<"Percentage Losses             = "
+            <<static_cast<float>(Losses)/nGames*PERCENT<<"%"<<endl;
+    
+    //Exit Stage Right!
+    in.close();
+    return 0;
+}
+
