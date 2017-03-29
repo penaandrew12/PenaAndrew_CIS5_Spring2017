d5c482757d2dacabd3b4ee77213bd876aa371529
penaandrew12 <penaandrew12@yahoo.com>
2017-03-29 09:09:36 -0700

    Signed-off-by: penaandrew12 <penaandrew12@yahoo.com>

---
+++ Homework/HW Assignment #4/Gaddis_8thEd_Ch5_Prob#1_SumNumbrs/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Pena
+ * Created on March 29, 2017, 9:10 PM
+ * Purpose: Sum of Numbers
+ */
+
+//System Libraries
+#include <iostream>  //Input - Output Library
+using namespace std; //Name-space under which system libraries exist
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare variables
+    
+    //Initialize variables
+    
+    //Input data
+    
+    //Map inputs to outputs or process the data
+    
+    //Output the transformed data
+    
+    //Exit stage!
+    return 0;
+}
+
---
+++ Homework/HW Assignment #4/Gaddis_8thEd_Ch5_Prob#1_SumNumbrs/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=gaddis_8thed_ch5_prob_1_sumnumbrs
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/gaddis_8thed_ch5_prob_1_sumnumbrs
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=gaddis8thedch5prob#1sumnumbrs.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/gaddis8thedch5prob#1sumnumbrs.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=gaddis_8thed_ch5_prob_1_sumnumbrs
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/gaddis_8thed_ch5_prob_1_sumnumbrs
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=gaddis8thedch5prob#1sumnumbrs.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/gaddis8thedch5prob#1sumnumbrs.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
