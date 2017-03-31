efb1e16e5b7ab92fa9f01338bdb463aedd2cbeb8
penaandrew12 <penaandrew12@yahoo.com>
2017-03-30 18:02:51 -0700

    Signed-off-by: penaandrew12 <penaandrew12@yahoo.com>

---
+++ Projects/Game_V1/main.cpp
@@ -0,0 +1,50 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Pena
+ * Created on March 30, 2017, 5:40 PM
+ * Purpose: Version 1 of Game
+ */
+
+//System Libraries 
+#include <iostream>
+using namespace std;
+
+//User Libraries 
+
+//Global Constants Only
+
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes
+
+//Program Execution Begins
+int main(int argc, char** argv) {
+    //Declare all Variables
+    int Mrbls;  //Symbols marbles for the materials for game
+    
+    //Input or initialize values
+    
+    cout<<"Congratulations! You get to play Dr. NIM!"<<endl; 
+    cout<<"The Objective of this game is to try to take the last marble."<<endl;
+    cout<<"The Rules are simple. You are to go first and you can choose One, Two, or Three marbles."<<endl;
+    cout<<"After you pick your marbles, The Computer will then have its turn to choose as well."<<endl;
+    cout<<"It will then let you go again and the process will repeat until the last marble is"<<endl;
+    cout<<"picked. Good Luck! Also, if you do not win the first time you may play again."<<endl;
+    cout<<"Enter The Amount of Marbles you wish to take = ";
+    cin>>Mrbls;
+    
+    cout<<"The Computer has decided to take One Marble."<<endl;
+    cout<<"The Computer has decided to take Two Marble."<<endl;
+    cout<<"The Computer has decided to take Three Marble."<<endl;
+    
+    cout<<"Enter The Amount of Marbles you wish to take = ";
+    cin>>Mrbls;
+    
+    
+    //Output Located
+    
+    
+    //Exit Stage
+    return 0;
+}
+
