cfa4843b1791a5ea6d69b0a182d335e7d9b9a804
penaandrew12 <penaandrew12@yahoo.com>
2017-04-01 14:45:53 -0700

    Signed-off-by: penaandrew12 <penaandrew12@yahoo.com>

--- Homework/HW Assignment #4/Gaddis_8thEd_Ch5_Prob#1_SumNumbrs/main.cpp
+++ Homework/HW Assignment #4/Gaddis_8thEd_Ch5_Prob#1_SumNumbrs/main.cpp
@@ -18,14 +18,21 @@ using namespace std; //Name-space under which system libraries exist
 //Execution begins here
 int main(int argc, char** argv) {
     //Declare variables
+    int num=0;
     
     //Initialize variables
     
     //Input data
+    cout<<"This Progarm will get the sum of the number you have entered starting from 1."<<endl;
+    cout<<"Enter a Real Number other than a Negative and 1."<<endl;
+    cout<<"If a Negative or 1 is Entered it will Not Compute."<<endl;
+    cin>>num;
     
     //Map inputs to outputs or process the data
     
     //Output the transformed data
+    for(num=0;num<=10;num++)
+        cout<<"If you were to add the sum from "<<num<<". It would be = "<<endl;
     
     //Exit stage!
     return 0;
--- Homework/HW Assignment #4/Gaddis_8thEd_Ch5_Prob#3_OcnLvls/main.cpp
+++ Homework/HW Assignment #4/Gaddis_8thEd_Ch5_Prob#3_OcnLvls/main.cpp
@@ -7,6 +7,7 @@
 
 //System Libraries
 #include <iostream>  //Input - Output Library
+#include <iomanip>   //Formatting Library
 using namespace std; //Name-space under which system libraries exist
 
 //User Libraries
@@ -18,15 +19,23 @@ using namespace std; //Name-space under which system libraries exist
 //Execution begins here
 int main(int argc, char** argv) {
     //Declare variables
+    int Count=0;    //Number to Start
     
     //Initialize variables
     
     //Input data
+    cout<<"This Program will calculate A Rising Sea Level Each Year"<<endl;
+    cout<<"Year          Amount Risen"<<endl;
+    cout<<"--------------------------"<<endl;
     
     //Map inputs to outputs or process the data
     
     //Output the transformed data
-    
+    for(Count=0;Count<=9;Count++)
+        cout<<Count<<" Years       "<<(Count*1.5f)<<" Millimeters"<<endl;
+    for(Count=10;Count<=25;Count++)
+        cout<<Count<<" Years      "<<(Count*1.5f)<<" Millimeters"<<endl;
+        
     //Exit stage!
     return 0;
 }
