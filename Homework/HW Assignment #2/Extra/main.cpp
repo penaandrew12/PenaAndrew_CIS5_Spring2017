04c800e05c0e8f015fcbf666d30b72acdb4c0da4
penaandrew12 <penaandrew12@yahoo.com>
2017-04-16 10:37:12 -0700

    Signed-off-by: penaandrew12 <penaandrew12@yahoo.com>

---
+++ Homework/HW Assignment #2/Extra/main.cpp
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Andrew
+ *
+ * Created on April 16, 2017, 10:31 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
+else if (T)
+            cout<<"The Computer takes 2 Marbles."<<endl;
+            Mrbls=Mrbls-2;
+            cout<<"There are "<<Mrbls<<" Marbles Left."<<endl<<endl;
+            }
+            else if (T)
+            cout<<"The Computer takes 3 Marbles."<<endl;
+            Mrbls=Mrbls-3;
+            cout<<"There are "<<Mrbls<<" Marbles Left."<<endl<<endl;
+            }
+            
+            else (F){{
+                RndmNum=(rand()% 3)+1;
+        cout<<"The Computer takes "<<RndmNum<<" Marble(s)."<<endl;
+        Mrbls=Mrbls-RndmNum;
+        cout<<"There are "<<Mrbls<<" Marbles Left."<<endl<<endl;
+        }
+                
+                    
+            
+    cout<<"Enter The Amount of Marbles you wish to take for Your Turn = ";
+    cin>>MrblsTkn;
+
+    Mrbls=Mrbls-MrblsTkn;
+    cout<<"You have taken "<<MrblsTkn<<" Marble(s). There are ";
+    cout<<Mrbls<<" Marble(s) Left."<<endl;
+    
+    if ((Mrbls-1)/4){
+        cout<<"The Computer takes 1 Marble."<<endl;
+        Mrbls=Mrbls-1;
+        cout<<"There are "<<Mrbls<<" Marbles Left."<<endl<<endl;
+        }
+        else if ((Mrbls-2)/4){
+            cout<<"The Computer takes 2 Marbles."<<endl;
+            Mrbls=Mrbls-2;
+            cout<<"There are "<<Mrbls<<" Marbles Left."<<endl<<endl;
+            }
+            else if ((Mrbls-3)/4){
+            cout<<"The Computer takes 3 Marbles."<<endl;
+            Mrbls=Mrbls-3;
+            cout<<"There are "<<Mrbls<<" Marbles Left."<<endl<<endl;
+            }
+    
+    cout<<"Enter The Amount of Marbles you wish to take for Your Turn = ";
+    cin>>MrblsTkn;
+
+    Mrbls=Mrbls-MrblsTkn;
+    cout<<"You have taken "<<MrblsTkn<<" Marble(s). There are ";
+    cout<<Mrbls<<" Marble(s) Left."<<endl<<endl;
+        
