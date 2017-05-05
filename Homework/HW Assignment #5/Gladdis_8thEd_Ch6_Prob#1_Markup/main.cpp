22b3f5af72ff1c987a6424c45a88f1556eaa2340
penaandrew12 <penaandrew12@yahoo.com>
2017-05-05 12:28:14 -0700

    Signed-off-by: penaandrew12 <penaandrew12@yahoo.com>

---
+++ Homework/HW Assignment #5/Gladdis_8thEd_Ch6_Prob#1_Markup/main.cpp
@@ -0,0 +1,54 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Pena
+ * Created on May 03, 2017, 11:30 AM
+ * Purpose: Mark-Up
+ */
+
+//System Libraries 
+#include <iostream>
+using namespace std;
+
+//User Libraries 
+
+//Global Constants Only
+//Like PI, e, Gravity, or conversions
+float PERCENT=100; //Percent Conversion
+
+//Function Prototypes
+float NewCost(float, float);
+
+//Program Execution Begins
+int main(int argc, char** argv) {
+    //Declare all Variables
+    float total, //Total
+          Perc, //Percent Markup
+          HSale, //Whole Sale Price
+          MarkUp; //Markup on Item
+    
+    //Input or initialize values
+    cout<<"Whole Sale on Item = $";
+    cin>>HSale;
+    cout<<"Mark Up Percent = ";
+    cin>>Perc;
+    
+    //Process/Calculations
+    MarkUp=HSale*(Perc/PERCENT);
+    
+    //Pull from Function
+    total=NewCost(HSale,MarkUp);
+    
+    //Output Located
+    cout<<"Markup is = $"<<MarkUp<<endl;
+    cout<<"New Price is = $"<<total<<endl;
+    
+    //Exit Stage
+    return 0;
+
+}
+
+float NewCost(float a, float b){
+    float c;
+    c=b+a;
+    return c;
+}
---
+++ Homework/HW Assignment #5/Gladdis_8thEd_Ch6_Prob#2_Area/main.cpp
@@ -0,0 +1,83 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Pena
+ * Created on May 03, 2017, 11:30 AM
+ * Purpose: Mark-Up
+ */
+
+//System Libraries 
+#include <iostream>
+using namespace std;
+
+//User Libraries 
+
+//Global Constants Only
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes
+float findHighest(float,float,float,float);
+
+//Program Execution Begins
+int main(int argc, char** argv) {
+    //Declare all Variables
+    float a,  //Sales for Quarter North
+          b,  //Sales for Quarter South
+          c,  //Sales for Quarter East
+          d;  //Sales for Quarter West
+    float high;
+    
+    //Input or initialize values
+    cout<<"Enter Sales for North quarter = $";
+    cin>>a;
+    if(a<0){
+        cout<<"Sales must be greater than 0.00 in Order to Compete"<<endl;
+        return(0);
+    }
+        
+    cout<<"Enter Sales for South quarter = $";
+    cin>>b;
+    if(b<0){
+        cout<<"Sales must be greater than 0.00 in Order to Compete"<<endl;
+        return(0);
+    }
+    
+    cout<<"Enter Sales for East quarter = $";
+    cin>>c;
+    if(c<0){
+        cout<<"Sales must be greater than 0.00 in Order to Compete"<<endl;
+        return(0);
+    }
+    
+    cout<<"Enter Sales for West quarter = $";
+    cin>>d;
+    if(d<0){
+        cout<<"Sales must be greater than 0.00 in Order to Compete"<<endl;
+        return(0);
+    }
+    
+    //Draw from Function
+    cout<<"The greatest sales is $";
+    high=findHighest(a,b,c,d);
+    cout<<endl;
+            
+    
+    //Exit Stage
+    return 0;
+
+}
+
+float findHighest(float a, float b, float c, float d){
+    
+    if(a>=b&&a>=c&&a>=d){
+    cout<<a<<". North Division Had the Highest Quarter Sales.";
+    }
+    if(b>=a&&b>=c&&b>=d){
+    cout<<b<<". South Division Had the Highest Quarter Sales.";
+    }
+    if(c>=a&&c>=b&&c>=d){
+    cout<<c<<". East Division Had the Highest Quarter Sales.";
+    }
+    if(d>=a&&d>=b&&d>=c){
+    cout<<d<<". West Division Had the Highest Quarter Sales.";
+    }
+}
